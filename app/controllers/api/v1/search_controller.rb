7b5b205a3dfd4fa7771ac51fc1d82f147023cb2d
github username <lhon001@gmail.com>
2019-12-16 23:57:52 -0500

    remove paywalled price endpoint from IEX

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,28 +1,31 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
+require 'config/keys'
+
 class Api::V1::SearchController < ApplicationController
 
+$base_URL = "https://cloud.iexapis.com/stable/stock/"
+$token = process.env.PUBLIC_TOKEN
+
   def show
-    # byebug
-    chart = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/chart")
+    chart = RestClient.get($base_URL + "#{search_params}/chart?token=" + $token)
     chart_resp = JSON.parse(chart)
 
-    stats = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/stats")
+    stats = RestClient.get($base_URL + "#{search_params}/stats?token=" + $token)
     stats_resp = JSON.parse(stats)
 
-    company = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/company")
+    company = RestClient.get($base_URL + "#{search_params}/company?token=" + $token)
     company_resp = JSON.parse(company)
 
-    logo = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/logo")
+    logo = RestClient.get($base_URL + "#{search_params}/logo?token=" + $token)
     logo_resp = JSON.parse(logo)
 
-    price = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/price")
-    price_resp = price.to_f
+    # !!!!! pricing data on IEX is now paywalled
+    # price = RestClient.get($base_URL + "#{search_params}/price" + token)
+    # price_resp = price.to_f
 
-    quote = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/quote")
+    quote = RestClient.get($base_URL + "#{search_params}/quote?token=" + $token)
     quote_resp = JSON.parse(quote)
-
-    # byebug
 
     formatted_json = {
       chart: chart_resp,
@@ -31,7 +34,7 @@ class Api::V1::SearchController < ApplicationController
       description: company_resp["description"],
       website: company_resp["website"],
       logo: logo_resp["url"],
-      price: price_resp,
+      # price: price_resp,
       marketcap: stats_resp["marketcap"],
       week52high: stats_resp["week52high"],
       week52low: stats_resp["week52low"],
@@ -54,7 +57,6 @@ class Api::V1::SearchController < ApplicationController
   end
 
   def get_news
-    # byebug
     news = RestClient.get("https://api.iextrading.com/1.0/stock/#{search_params}/news")
 
     render json: news
